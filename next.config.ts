afa36956a167d4a3305bb2b033b2e24d1700508b
MuhammadHanzala31 <hanzala.sispn@gmail.com>
2026-02-26 00:01:34 +0500

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   images: {
+    domains: ["www.onlinequrancourse.com"],
+  },
+  output: 'export'
   /* config options here */
 };
 
--- src/app/components/blogs/BlogCard.tsx
+++ src/app/components/blogs/BlogCard.tsx
@@ -2,17 +2,19 @@ import Image from 'next/image'
 import React from 'react'
 import cardImage from '@/../public/blog-card.png'
 
-export default function BlogCard({flexDirection} : {flexDirection? : string}) {
+export default function BlogCard({ flexDirection, image, title, date, author, expert }: { flexDirection?: string, image?: string, title?: string, author?: string, expert?: string, date?: string }) {
   return (
-    <div className={`flex flex-col gap-4 items-center ${flexDirection? flexDirection : 'lg:flex-row'}`}>
-      <Image src={cardImage} alt='sorry'/>
+    <div className={`flex flex-col gap-4 items-center ${flexDirection ? flexDirection : 'lg:flex-row'}`}>
+      <Image src={image ? image : cardImage} alt='sorry' />
       <div className='flex flex-col gap-3.5'>
         <div className='flex gap-6'>
-          <h6 className='text-black font-semibold text-[12px] font-poppins'>admin</h6>
-          <h6 className='text-black font-semibold text-[12px] font-poppins'>January 3, 2023</h6>
+          <h6 className='text-black font-semibold text-[12px] font-poppins'>{author}</h6>
+          <h6 className='text-black font-semibold text-[12px] font-poppins'>{date}</h6>
         </div>
-        <h3 className='text-[#098A46] text-[24px] font-medium font-poppins'>Prophethood and the last Prophet SWS</h3>
-        <p className='text-[#535353] font-poppins text-[16px] w-[90%]'>Prophethood refers to the belief in a religious tradition that certain individuals were chosen by God to be messengers or prophets who were tasked </p>
+        {/* <h3 className='text-[#098A46] text-[24px] font-medium font-poppins' dangerouslySetInnerHTML={{
+          __html : title
+        }}></h3> */}
+        <p className='text-[#535353] font-poppins text-[16px] w-[90%]'>{expert}</p>
       </div>
     </div>
   )
