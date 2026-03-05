5cd85566f166d96412cd2f95a7268af1da1b06e9
MuhammadHanzala31 <hanzala.sispn@gmail.com>
2026-03-06 03:08:55 +0500

    change images

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import Image from 'next/image'
 import { useState } from 'react'
+import img from '@/../public/registration.png'
 
 export default function page() {
     const [formData, setFormData] = useState({
@@ -40,47 +42,11 @@ export default function page() {
                         </div>
 
                         {/* Steps Visualization */}
-                        <div className="relative h-screen flex items-center justify-between gap-8   ">
-                            {/* STEP 01 */}
-                            <div className="flex flex-col items-center gap-3">
-                                <div className="px-4 py-2 border-2 w-fit rounded-3xl border-[#7AC043] font-sans text-[#098A46] font-bold">
-                                    STEP 01
-                                </div>
-                                <svg xmlns="http://www.w3.org/2000/svg" height={100} xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_2_step_01" x="0px" y="0px" viewBox="0 0 511.3 700.6" xmlSpace="preserve">
-                                    <path fill="#098A46" d="M7.4,110.2v506.7l189,71.4c27.6,10.4,58,10.5,85.7,0.3L506,606.1V100.7l-203.9,86  c-29.2,12.3-61.9,12.8-91.5,1.5L7.4,110.2L7.4,110.2z"></path>
-                                    <path fill="#7AC043" d="M206.7,16.4L7.4,110.2l203.3,78c29.5,11.3,62.3,10.8,91.5-1.5l203.9-86L307.4,14.9  C275.1,0.9,238.5,1.5,206.7,16.4L206.7,16.4z"></path>
-                                </svg>
-                                <p className="text-sm font-semibold text-gray-800 text-center">Get register yourself by submitting the form.</p>
-                            </div>
-
-                            {/* STEP 02 */}
-                            <div className="flex flex-col items-center gap-3">
-                                <div className="px-4 py-2 w-fit border-2 rounded-3xl border-[#7AC043] font-sans text-[#098A46] font-bold">
-                                    STEP 02
-                                </div>
-                                <svg xmlns="http://www.w3.org/2000/svg" height={200} xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_2_step_02" x="0px" y="0px" viewBox="0 0 511.3 700.6" xmlSpace="preserve">
-                                    <path fill="#098A46" d="M7.4,110.2v506.7l189,71.4c27.6,10.4,58,10.5,85.7,0.3L506,606.1V100.7l-203.9,86  c-29.2,12.3-61.9,12.8-91.5,1.5L7.4,110.2L7.4,110.2z"></path>
-                                    <path fill="#7AC043" d="M206.7,16.4L7.4,110.2l203.3,78c29.5,11.3,62.3,10.8,91.5-1.5l203.9-86L307.4,14.9  C275.1,0.9,238.5,1.5,206.7,16.4L206.7,16.4z"></path>
-                                </svg>
-                                <p className="text-sm font-semibold text-gray-800 text-center">Experience our one-week free trial classes online.</p>
-                            </div>
-
-                            {/* STEP 03 */}
-                            <div className="flex flex-col justify-center items-center gap-3">
-                                <div className="px-4 py-2 border-2 w-fit  rounded-3xl border-[#7AC043] font-sans text-[#098A46] font-bold">
-                                    STEP 03
-                                </div>
-                                <svg xmlns="http://www.w3.org/2000/svg" height={280} xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_2_step_03" x="0px" y="0px" viewBox="0 0 511.3 700.6" xmlSpace="preserve">
-                                    <path fill="#098A46" d="M7.4,110.2v506.7l189,71.4c27.6,10.4,58,10.5,85.7,0.3L506,606.1V100.7l-203.9,86  c-29.2,12.3-61.9,12.8-91.5,1.5L7.4,110.2L7.4,110.2z"></path>
-                                    <path fill="#7AC043" d="M206.7,16.4L7.4,110.2l203.3,78c29.5,11.3,62.3,10.8,91.5-1.5l203.9-86L307.4,14.9  C275.1,0.9,238.5,1.5,206.7,16.4L206.7,16.4z"></path>
-                                </svg>
-                                <p className="text-sm font-semibold text-gray-800 text-center">Once satisfied, enroll in regular classes.</p>
-                            </div>
-                        </div>
+                        <Image src={img} alt='regis'/>
                     </div>
 
                     {/* Right Side - Form */}
-                    <div className="bg-white rounded-3xl w-[90%]  shadow-lg p-8 border border-gray-200">
+                    <div className="bg-white rounded-none w-[90%]  shadow-lg p-8 border border-[#098A46]">
                         <h2 className="text-2xl text-center font-sans font-bold text-[#098A46] mb-4">
                             Start Your Free Trial Lesson Now.
                         </h2>
@@ -88,9 +54,7 @@ export default function page() {
                         <form onSubmit={handleSubmit} className="space-y-2">
                             {/* Name */}
                             <div>
-                                <label className="block text-sm font-semibold text-gray-900 mb-2">
-                                    Name <span className="text-red-500">*</span>
-                                </label>
+                             
                                 <input
                                     type="text"
                                     name="name"
@@ -104,9 +68,7 @@ export default function page() {
 
                             {/* Email */}
                             <div>
-                                <label className="block text-sm font-semibold text-gray-900 mb-2">
-                                    Email <span className="text-red-500">*</span>
-                                </label>
+                              
                                 <input
                                     type="email"
                                     name="email"
@@ -120,9 +82,7 @@ export default function page() {
 
                             {/* Phone */}
                             <div>
-                                <label className="block text-sm font-semibold text-gray-900 mb-2">
-                                    Enter Your Phone <span className="text-red-500">*</span>
-                                </label>
+                             
                                 <input
                                     type="tel"
                                     name="phone"
@@ -136,9 +96,7 @@ export default function page() {
 
                             {/* City & Country */}
                             <div>
-                                <label className="block text-sm font-semibold text-gray-900 mb-2">
-                                    Enter Your City & Country <span className="text-red-500">*</span>
-                                </label>
+                            
                                 <input
                                     type="text"
                                     name="city"
@@ -152,9 +110,7 @@ export default function page() {
 
                             {/* Message */}
                             <div>
-                                <label className="block text-sm font-semibold text-gray-900 mb-2">
-                                    How can we help you? (optional)
-                                </label>
+
                                 <textarea
                                     name="message"
                                     placeholder="Message"
@@ -186,7 +142,7 @@ export default function page() {
                             {/* Submit Button */}
                             <button
                                 type="submit"
-                                className="w-full bg-[#098A46] hover:bg-[#077a3d] text-white font-bold py-3 px-6 rounded-full transition-colors duration-200"
+                                className="w-full bg-[#098A46] hover:bg-[#077a3d] text-white font-bold py-3 px-6 rounded-none transition-colors duration-200"
                             >
                                 Submit
                             </button>
