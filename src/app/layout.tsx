0f60960933a72a105c926136bcd4284664ef3a74
MuhammadHanzala31 <hanzala.sispn@gmail.com>
2026-07-29 20:19:23 +0500

    add schemas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import 'remixicon/fonts/remixicon.css'
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 
 
@@ -35,7 +36,7 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Online Quran Course",
   description: "developed by zia khalid",
-  
+
 };
 
 export default function RootLayout({
@@ -43,15 +44,98 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const organizationSchema = {
+    "@context": "https://schema.org",
+    "@type": "EducationalOrganization",
+    "name": "Online Quran Course",
+    "alternateName": "OQC Academy",
+    "url": "https://onlinequrancourse.com/",
+    "logo": "https://onlinequrancourse.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FOnline%20Quran%20Logo%20Final.8bc36ea6.png&w=128&q=75",
+    "sameAs": [
+      "https://www.facebook.com/onlinequrancourse/",
+      "https://x.com/OnlineQC/",
+      "https://www.instagram.com/onlinequrancourse"
+    ]
+  }
+
+
+  const websiteSchema = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "@id": "https://onlinequrancourse.com/#website",
+    "url": "https://onlinequrancourse.com/",
+    "name": "Online Quran Course",
+    "alternateName": "OQC Academy",
+    "description":
+      "Online Quran Course offers one-on-one Quran classes for kids and adults with experienced male and female Quran teachers. Learn Quran reading, Tajweed, Quran memorization, Tafseer, and Islamic studies online from anywhere in the world.",
+    "inLanguage": "en",
+    "publisher": {
+      "@type": "Organization",
+      "name": "Online Quran Course",
+      "url": "https://onlinequrancourse.com/"
+    },
+    "hasPart": [
+      {
+        "@type": "WebPage",
+        "name": "Home",
+        "url": "https://onlinequrancourse.com/"
+      },
+      {
+        "@type": "CollectionPage",
+        "name": "All Courses",
+        "url": "https://onlinequrancourse.com/all-courses"
+      },
+      {
+        "@type": "AboutPage",
+        "name": "About Us",
+        "url": "https://onlinequrancourse.com/about-us"
+      },
+      {
+        "@type": "Blog",
+        "name": "Blogs",
+        "url": "https://onlinequrancourse.com/blogs"
+      },
+      {
+        "@type": "CollectionPage",
+        "name": "All Teachers",
+        "url": "https://onlinequrancourse.com/all-teachers"
+      },
+      {
+        "@type": "ContactPage",
+        "name": "Contact Us",
+        "url": "https://onlinequrancourse.com/contact-us"
+      }
+    ]
+  };
+
+
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${jakarta.variable} ${poppins.variable} antialiased`}
       >
-     <Navbar/>
-     <Toaster/>
+        <Script
+            id="website-schema"
+            type="application/ld+json"
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify(websiteSchema),
+            }}
+          />
+          <Script
+            id="organization-schema"
+            type="application/ld+json"
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify(organizationSchema),
+            }}
+          />
+        <Navbar />
+        <Toaster />
         {children}
-      <Footer/>
+        <Footer />
       </body>
     </html>
   );
